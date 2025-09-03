173b5dfd1b22d74e47972b1f66b3ec58cd91c3b2
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2025-09-03 22:35:31 +0930

    is working

--- zzz-backups.bat
+++ zzz-backups.bat
@@ -14,11 +14,11 @@ if %EL% neq 0 (
 )
 
 REM -PurgeRestorePoints -NoPurgeRestorePoints -CleanupBeforehand -NoCleanupBeforehand -Berbose
-REM powershell -NoProfile -ExecutionPolicy Bypass -File "C:\000-Essential-tasks\zzz-backups_06.ps1" -Target_Drives_List "D: E: F: G: T: I: Y:" -Headroom_PCT 35 -NoCleanupBeforehand -PurgeRestorePoints
+REM powershell -NoProfile -ExecutionPolicy Bypass -File "C:\000-Essential-tasks\zzz-backups_07.ps1" -Target_Drives_List "D: E: F: G: T: I: Y:" -Headroom_PCT 35 -NoCleanupBeforehand -PurgeRestorePoints
 
-REM powershell -NoProfile -ExecutionPolicy Bypass -File "C:\000-Essential-tasks\zzz-backups_06.ps1" -Target_Drives_List "D:" -Headroom_PCT 35 -NoCleanupBeforehand -PurgeRestorePoints
+REM powershell -NoProfile -ExecutionPolicy Bypass -File "C:\000-Essential-tasks\zzz-backups_07.ps1" -Target_Drives_List "D:" -Headroom_PCT 35 -NoCleanupBeforehand -PurgeRestorePoints
 
-powershell -NoProfile -ExecutionPolicy Bypass -File "C:\000-Essential-tasks\zzz-backups_06.ps1" -Target_Drives_List "D: G:" -Headroom_PCT 35 -CleanupBeforehand -PurgeRestorePoints
+powershell -NoProfile -ExecutionPolicy Bypass -File "C:\000-Essential-tasks\zzz-backups_07.ps1" -Target_Drives_List  "D: E: F: G: T: I: Y:" -Headroom_PCT 35 -CleanupBeforehand -PurgeRestorePoints
 
 pause
 goto :eof
